fddf20c38d2a1ea6905e06707d5c7dee4ddac75c
BernatLl <blloret@hotmail.com>
2022-04-05 07:47:39 +0000

    1st update

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+// Domain Generator //
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+const domaingenerator = () => {
+  for (let p = 0; p < pronoun.length; p++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        let domain = `${pronoun[p]}${adj[a]}${noun[n]}.com`;
+        document.write(
+          `<html><body><p class="text-center">${domain}</p>></body></html>`
+        );
+      }
+    }
+  }
+
+  return;
 };
+domaingenerator();
